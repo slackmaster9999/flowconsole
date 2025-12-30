51ca07a7cf65971ed607d58a8833171fcd644c9f
vg <v@sd.sd>
2025-12-30 13:13:45 +0300

    fix(ci): add NEXT_PUBLIC_BASE_PATH environment variable for deployment

--- src/docs/next.config.js
+++ src/docs/next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
     remotePatterns: [
