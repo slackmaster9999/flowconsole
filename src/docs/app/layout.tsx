340935e8666e8383f4781e93a31988112c402f5c
vg <v@sd.sd>
2026-01-02 12:40:43 +0300

    chore(docs): removed vercel analytics component from layout.tsx

--- src/docs/app/layout.tsx
+++ src/docs/app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             disableTransitionOnChange
           >
             <ModalProvider>{children}</ModalProvider>
-            <Analytics />
           </ThemeProvider>
       </body>
     </html>
