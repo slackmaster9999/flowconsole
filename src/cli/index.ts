d7e5f66ae287741769cd869b5c94684a1bd7f191
vg <v@sd.sd>
2026-01-15 21:27:53 +0300

    fix(cli): fixed additinal langs import

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,8 +1,8 @@
-import { parse, registerDynamicLanguage } from '@ast-grep/napi';
-import csharp from '@ast-grep/lang-csharp';
+import { Lang, parse, registerDynamicLanguage } from '@ast-grep/napi';
+//import csharp from '@ast-grep/lang-csharp';
+//registerDynamicLanguage({ csharp });
 
-registerDynamicLanguage({ csharp });
-const ast = parse("csharp", "var s = 1; Console.WriteLine(s);");
+const ast = parse(Lang.TypeScript, "var s = 1; Console.WriteLine(s);");
 const root = ast.root()
 var result = root.find("Console.WriteLine");
 console.log(result);
