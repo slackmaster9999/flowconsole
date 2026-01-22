412146417b5d6acdd62eb0809e08db4dcbd95ea4
vg <v@sd.sd>
2026-01-22 16:52:51 +0300

    chore: import fix + deps update

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeProvider } from 'flowconsole/theme/ThemeProvider.tsx'
+import { ThemeProvider } from 'flowconsole/theme/ThemeProvider';
 import { PostHogProvider } from 'posthog-js/react';
 
 const options = {
